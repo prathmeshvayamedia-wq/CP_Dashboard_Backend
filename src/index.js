89b89aef94fa9eb88fb650cdf530e9c326c2829e
prathmeshvayamedia-wq <prathmesh.vayamedia@gmail.com>
2026-05-30 15:43:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.set('trust proxy', 1);
 
 app.use(helmet());
-app.use(cors(...));
+// app.use(cors(...));
 
 // app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
 
